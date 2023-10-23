03ea8d23eca92ea122ebe1ebd7c21c3864a33421
骆言 <lawsssscat@163.com>
2023-10-23 23:35:23 +0800

    Update csdn_restrictless.user.js

--- user.js/csdn_restrictless.user.js
+++ user.js/csdn_restrictless.user.js
@@ -27,7 +27,24 @@
     GM_addStyle('#content_views pre code{user-select:text !important}');
     GM_addStyle('#content_views pre{user-select:text !important}');
     GM_addStyle('#content_views{user-select:text !important}');
-    $("#content_views").unbind("copy"); # 禁止：复制事件时，登录弹窗+事件阻止
+    setTimeout(() => {
+        // $("#content_views").unbind("copy"); // 禁止：复制事件时，登录弹窗+事件阻止
+        // 复制功能
+        document.querySelector('.blog-content-box').addEventListener("copy", (e) => {
+            e.stopPropagation()
+            e.preventDefault()
+            navigator.clipboard.writeText(window.getSelection().toString())
+        }, true)
+        document.addEventListener("keydown", (e) => {
+            if (e.ctrlKey && e.keyCode == 67) {
+                e.stopPropagation()
+                e.preventDefault()
+                navigator.clipboard.writeText(window.getSelection().toString())
+            }
+        }, true);
+        document.oncopy = null
+        window.oncopy = null
+    },500);
     // 文字复制按钮免登录（代码框，右上角，鼠标移入时显示）
     // todo
 
